3fba33b04447a69576166b72f153b2bbb6769f8c
Iris-hl-lau <98.iris.lau@gmail.com>
2018-09-14 20:03:41 -0700

    GCD function from iris

---
+++ gcd.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Iris L on 2018-09-14.
+//
+
+#ifndef HELLOLION_GCD_HPP
+#define HELLOLION_GCD_HPP
+// Greatest Common Denominator
+// PRE : a is a positive integer
+// PRE : b is a positive integer
+// POST : a and b are unchanged
+// RETURN : the greatest common denominator of
+// a and b .
+int gcd (const int a, const int b);
+#endif //HELLOLION_GCD_HPP
--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include "gcd.hpp"
+
+using namespace std;
+
+int gcd(const int a , const int b){
+
+    if(a == b) {
+        return a;
+    }else {
+        if(a > b) {
+            for (int i = b; i > 1; i--) {
+                if(a%i==0 && b%i==0) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = a; i > 1; i--) {
+                if(a%i==0 && b%i==0) {
+                    return i;
+                }
+            }
+        }
+    }
+    return 1;
+}
 
 int main() {
-    std::cout << "Hello from the other side!" << std::endl;
+    const int m = 24;
+    const int n = 108;
+
+    const int o = 10;
+    const int p = 10;
+
+    const int r = 182;
+    const int s = 52;
+
+    const int z = 23;
+    const int y = 6;
+
+    cout << "Greatest common denominator: " << gcd(m, n) << endl;
+    cout << "Greatest common denominator: " << gcd(p, o) << endl;
+    cout << "Greatest common denominator: " << gcd(r, s) << endl;
+    cout << "Greatest common denominator: " << gcd(y, z) << endl;
+
+    cout << "Hello from the other side!" << endl;
     return 0;
 }
